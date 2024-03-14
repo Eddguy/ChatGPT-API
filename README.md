4ec5710311480a3b39569be20c0e3f7462afa22c
eddguy <ecubasmarine2019@fau.edu>
2024-03-14 18:19:41 -0400

    additional dependecies

---
+++ README.md
@@ -0,0 +1,7 @@
+This Project is a sample of how to do API calls with ChatGPT
+
+* Makes sure you use the following command to get the program to run
+
+` npm install all `
+
+-Eddie Cubas
---
+++ chatGPTConnect.js
@@ -0,0 +1,10 @@
+/************************************************************************************************************/
+// chatGPTConnect.js
+// Eddie Cubas
+// This is a sample of how to do API calls to chatGPT using node.js and swagger
+/************************************************************************************************************/
+
+const express = require('express');
+const app = express();
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
